6a54a41722d371d6a8afa83a143dd1d4e5274ed0
salamandery <sp.ront@hotmal.com>
2021-12-31 07:54:27 -0300

    remove hook

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { MultiStepProvider } from './MultiStepContext';
 import { ToastProvider } from './ToastContext';
-import { AuthProvider } from './AuthContext';
 
 const AppProvider: React.FC = ({ children }) => {
   return (
-    <AuthProvider>
-      <MultiStepProvider>
-        <ToastProvider>{children}</ToastProvider>
-      </MultiStepProvider>
-    </AuthProvider>
+    <MultiStepProvider>
+      <ToastProvider>{children}</ToastProvider>
+    </MultiStepProvider>
   );
 };
 
